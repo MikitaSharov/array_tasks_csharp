e7613e729f471c09a17579aa9a64205ecc853ab4
Nikita Sharov <lliapik@gmail.com>
2023-08-15 15:36:36 +0300

    task001

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,11 +1,13 @@
-﻿// Метод по заполнению массива.
+﻿int[] userarray = GetRandomArray(10); //10 - размер массива
+int[] evenarray = GetEvenArray(userarray);
+int[] oddarray = GetOddArray(userarray);
+// Метод по заполнению массива.
 int[] GetRandomArray(int length)
 {
     int[] result = new int[length];
     for (int i = 0; i < result.Length; i++)
     {
         result[i] = new Random().Next(1, 10);
-
     }
     return result;
 }
@@ -16,42 +18,64 @@ void PrintArray(int[] arraytoprint)
     for (int i = 0; i < arraytoprint.Length; i++)
     {
         Console.Write(arraytoprint[i]);
-        if (i!= arraytoprint.Length-1)
+        if (i != arraytoprint.Length - 1)
         {
             Console.Write(", ");
         }
     }
-    Console.Write("]");
+    Console.WriteLine("]");
 }
-int[] userarray = GetRandomArray(10);
-PrintArray(userarray);
+
+// Метод по заполнению массива чётными значениями из другого массива
 int[] GetEvenArray(int[] array)
 {
-    int even = 0;
+    // Считаем кол-во четных элементов в массиве userarray и это будет размером нового массива
+    int evenCount = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i]%2==0)
+        if (array[i] % 2 == 0)
         {
-            even++;
+            evenCount++;
         }
-
     }
-    int[] result = new int[even];
-    int[] result1 = new int[array.Length-even];
-    for (int i = 0; i < even; i++)
+    int[] evenarray = new int[evenCount]; //создаём новый массив размером evenCount
+
+    // заполняем новые значения evenarray[index] чётными значениями проверяя каждое значение item входного массива 
+    int index = 0;
+    foreach (int item in array)
     {
-        if (array[i]%2==0)
+        if (item % 2 == 0)
         {
-            result[i]=array[i];
-        }
-        else
-        {
-            result1[i]= array[i];
+            evenarray[index] = item;
+            index++;
         }
     }
-    return result;
-    return result1;
-
+    return evenarray;
 }
-int[] evenarray = GetEvenArray(userarray);
+
+// Метод по заполнению массива нечётными значениями из другого массива
+int[] GetOddArray(int[] array)
+{
+    int oddCount = 0;
+    foreach (int item in array)
+    {
+        if (item%2==1) oddCount++;
+    }
+
+    int[] oddarray = new int[oddCount];
+
+    int index = 0;
+    foreach (int item in array)
+    {
+        if (item%2==1)
+        {
+            oddarray[index] = item;
+            index++;
+        }
+    }
+    return oddarray;
+}
+
+PrintArray(userarray);
 PrintArray(evenarray);
+PrintArray(oddarray);
