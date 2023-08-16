ce4517cad9345f71be3e57a32f8f670ed8f6ff4c
Nikita Sharov <lliapik@gmail.com>
2023-08-16 15:35:31 +0300

    FillArrayFromConsole

---
+++ task007/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите методы для ввода одномерного массива с клавиатуры и для вывода его содержимого на экран.
+
+int[] array = FillArrayFromConsole();
+
+int[] FillArrayFromConsole()
+{
+    Console.Write("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+}
+void PrintArray(int[] arrayForPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        Console.Write(arrayForPrint[i]);
+        if (i < arrayForPrint.Length - 1) System.Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+PrintArray(array);
