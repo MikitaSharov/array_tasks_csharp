b636ef8d478979c7d6ebba29f593c518bcf76cb5
Nikita Sharov <lliapik@gmail.com>
2023-08-16 13:03:06 +0300

    max-min из double array

---
+++ task006/Program.cs
@@ -0,0 +1,44 @@
+﻿// В заданном массиве действительных чисел найдите разность между максимальным и минимальным числом.
+
+double[] array = FillDoubleArray(10, -100, 100);
+
+double[] FillDoubleArray(int size, double minValue, double maxValue)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+    }
+
+    return array;
+}
+void PrintArray(double[] arraytoprint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arraytoprint.Length; i++)
+    {
+        Console.Write(Math.Round(arraytoprint[i], 2));
+        if (i != arraytoprint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+double min = array[0];
+double max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    else if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+PrintArray(array);
+Console.WriteLine($"max = {max:N2}, min = {min:N2}, max-min = {(max - min):N2}");
