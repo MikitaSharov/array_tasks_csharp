b8ccd1a767287f89d38f1baa185cc4dd54f03119
Nikita Sharov <lliapik@gmail.com>
2023-08-17 12:08:59 +0300

    task009

---
+++ task009/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Сформируйте одномерный массив действительных чисел как геометрическую последовательность со знаменателем ½,
+начиная с 1, т.е.: 1,0 0,5 0,25 0,125 и т.д. */
+
+Console.Write("Enter array length: ");
+int length = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[length];
+array[0] = 1;
+
+for (int i = 1; i < length; i++)
+{
+    array[i] = array[i-1]/2;
+}
+
+Console.WriteLine(string.Join(", ", array));
